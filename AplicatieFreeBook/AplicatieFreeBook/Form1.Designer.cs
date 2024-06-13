2920e2c34b13f94ba5c6066796a5d0a05b40e112
Vlosta <vladut.ostasuc@gmail.com>
2024-06-13 10:11:39 +0300

    Initial commit

---
+++ AplicatieFreeBook/AplicatieFreeBook/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace AplicatieFreeBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(29, 50);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(720, 352);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(712, 326);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Carti disponibile";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(700, 314);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(712, 326);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Carti imprumutate";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(604, 317);
+            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.DataSourceChanged += new System.EventHandler(this.dgv2_source);
+            this.dataGridView2.DefaultCellStyleChanged += new System.EventHandler(this.dgv2);
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(712, 326);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Statistici biblioteca";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Meniu FreeBook";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ AplicatieFreeBook/AplicatieFreeBook/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+using System.IO;
+
+namespace AplicatieFreeBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\freebook.mdf;Integrated Security=True;Connect Timeout=30");
+        string path = "";
+        string emailUserLogat = "ion@yahoo.com";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Email user: " + emailUserLogat; 
+            path = System.IO.Directory.GetCurrentDirectory().ToString();
+            path += "\\csarp\\";
+            con.Open();
+            SqlCommand cmdTrunc1 = new SqlCommand("Truncate table utilizatori", con);
+            cmdTrunc1.ExecuteNonQuery();
+            SqlCommand cmdTrunc2 = new SqlCommand("Truncate table carti", con);
+            cmdTrunc2.ExecuteNonQuery();
+            StreamReader sr = new StreamReader(path + "\\utilizatori.txt");
+            SqlCommand cmdTrunc3 = new SqlCommand("Truncate table imprumut", con);
+            cmdTrunc3.ExecuteNonQuery();
+            string line="";
+            while((line=sr.ReadLine()) != null)
+            {
+                string email = line.Split('*')[0];
+                string parola = line.Split('*')[1];
+                string nume = line.Split('*')[2];
+                string prenume = line.Split('*')[3];
+                SqlCommand cmd1 = new SqlCommand("Insert into utilizatori values(@p1,@p2,@p3,@p4)", con);
+                cmd1.Parameters.Add("@p1", email);
+                cmd1.Parameters.Add("@p2", parola);
+                cmd1.Parameters.Add("@p3", nume);
+                cmd1.Parameters.Add("@p4", prenume);
+                cmd1.ExecuteNonQuery();
+
+            }
+            sr = new StreamReader(path + "\\carti.txt");
+            line = "";
+            while ((line = sr.ReadLine()) != null)
+            {
+                string titlu = line.Split('*')[0];
+                string autor = line.Split('*')[1]; 
+                string gen = line.Split('*')[2];
+                SqlCommand cmd1 = new SqlCommand("Insert into carti values(@p1,@p2,@p3)", con);
+                cmd1.Parameters.Add("@p1", titlu);
+                cmd1.Parameters.Add("@p2", autor);
+                cmd1.Parameters.Add("@p3", gen);
+                cmd1.ExecuteNonQuery();
+            }
+            sr = new StreamReader(path + "\\imprumuturi.txt");
+            line = "";
+            while((line=sr.ReadLine()) != null)
+            {
+                string titlu = line.Split('*')[0]; 
+                string email = line.Split('*')[1];
+                DateTime data = DateTime.Parse(line.Split('*')[2]);
+                SqlCommand cmd1 = new SqlCommand("Select id_carte from carti where titlu=@p1",con);
+                cmd1.Parameters.Add("@p1", titlu);
+                SqlDataReader rdr=cmd1.ExecuteReader();
+                rdr.Read();
+                int id_carte = Convert.ToInt32(rdr[0]);
+                rdr.Close();
+                SqlCommand cmd2 = new SqlCommand("Insert into imprumut values(@p1,@p2,@p3)", con);
+                cmd2.Parameters.Add("@p1", id_carte);
+                cmd2.Parameters.Add("@p2", email);
+                cmd2.Parameters.Add("@p3", data);
+                cmd2.ExecuteNonQuery();
+
+            }
+            con.Close();
+
+            SqlDataAdapter sda = new SqlDataAdapter("Select distinct carti.id_carte, carti.titlu,carti.autor,carti.gen from carti inner join imprumut on carti.id_carte=imprumut.id_carte and imprumut.email='"+emailUserLogat+"'", con);
+            DataTable dt1=new DataTable();
+            sda.Fill(dt1);
+            dataGridView1.DataSource = dt1;
+            DataGridViewButtonColumn coloana = new DataGridViewButtonColumn();
+            coloana.Name = "Imprumuta carte";
+            dataGridView1.Columns.Add(coloana);
+            dataGridView1.Columns[0].Visible = false;
+
+            DataTable dt2 = new DataTable();
+            DataColumn c1 = new DataColumn();
+            SqlDataAdapter sda1 = new SqlDataAdapter("Select carti.id_carte, carti.titlu, carti.autor, imprumut.data_imprumut from carti inner join imprumut on carti.id_carte=imprumut.id_carte and imprumut.email='" + emailUserLogat + "'", con);
+            sda1.Fill(dt2);
+            for (int i = 0; i < dt2.Rows.Count; i++)
+                dt2.Rows[i][0] = i + 1;
+            dt2.Columns.Add("Data disponibilitate");
+            for (int i = 0; i < dt2.Rows.Count; i++)
+                dt2.Rows[i][4] =  Convert.ToDateTime(dt2.Rows[i][3]).AddDays(30).ToShortDateString();
+             
+            dataGridView2.DataSource = dt2;
+             
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            label1.Text = e.ColumnIndex.ToString();
+            //dataGridView1.Columns[e.ColumnIndex].DefaultCellStyle.BackColor = Color.Red;
+        }
+
+        private void dgv2(object sender, EventArgs e)
+        {
+            dataGridView2[1, 1].Style.BackColor = Color.Red;
+        }
+
+        private void click(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView2[1, 1].Style.BackColor = Color.Red;
+
+        }
+
+        private void dgv2_source(object sender, EventArgs e)
+        {
+            DataTable dt2 = new DataTable();
+            dt2 = (DataTable)dataGridView2.DataSource;
+            DataGridViewCellStyle style = new DataGridViewCellStyle();
+            style.BackColor = Color.Red;
+            for (int i = 0; i < dt2.Rows.Count; i++)
+            {
+                dataGridView2[1, i].Style = style;
+            }
+        }
+    }
+}
